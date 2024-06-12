1d21356236ae7c546b4993f949e4f0f938966696
20245096 <168728053+20245096@users.noreply.github.com>
2024-06-12 13:07:40 +0900

    print_stars
    별 모양을 출력하는 프로그램입니다.

---
+++ #include<stdio.h>.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void print_stars()
+{
+    for (int i=0; i<10; i++){
+        printf("*");
+        }
+        printf("\n");
+}
+
+
+int main() 
+{
+    print_stars();
+    print_stars();
+    return 0;
+}
